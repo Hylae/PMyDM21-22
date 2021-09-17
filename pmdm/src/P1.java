b77fedccd64ac360e23ac2a9d5fd971e8b659852
David <davidromeral@hotmail.com>
2021-09-17 16:47:33 +0200

    Primera version

--- pmdm/src/P1.java
+++ pmdm/src/P1.java
@@ -1,7 +1,12 @@
 public class P1 {
+    String sNombre = "alb erto fer alf al7 dez dez alb san chez";
+    String arraySeccion[] = sNombre.split(" ");
 
-    public IllegalAccessError getError() {
-        return error;
+
+    for(int i = 0;i<arraySeccion.length;i++)
+    {
+        if(arraySeccion[i].length()%2!=0);
     }
+
 }
 
